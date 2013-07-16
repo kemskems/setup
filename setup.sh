62b40b1c1c175130f6d743df5bc7c70173e8fa85
Kemal Maulana <kemskems12@gmail.com>
2013-07-17 02:07:50 +0800

    Add -y option for installation

--- setup.sh
+++ setup.sh
@@ -7,22 +7,22 @@
 sudo apt-get update
 
 # Install packages from official repository
-sudo apt-get install vim
-sudo apt-get install php5
-sudo apt-get install openjdk-7-jdk
-sudo apt-get install gcc
-sudo apt-get install vlc
-sudo apt-get install byobu
+sudo apt-get -y install vim
+sudo apt-get -y install php5
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install gcc
+sudo apt-get -y install vlc
+sudo apt-get -y install byobu
 
 # Add third-party repositories (PPA)
 # Sublime Text 2
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 # MySQL using MariaDB
-sudo apt-get install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb http://mariadb.biz.net.id//repo/10.0/ubuntu raring main'
 # NodeJS
-sudo apt-get install python-software-properties python g++ make
+sudo apt-get -y install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 # Spotify
 sudo add-apt-repository 'deb http://repository.spotify.com stable non-free'
@@ -30,13 +30,13 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 
 # Update and install
 sudo apt-get update
-sudo apt-get install sublime-text
-sudo apt-get install mariadb-server
-sudo apt-get install nodejs npm
-sudo apt-get install spotify-client
+sudo apt-get -y install sublime-text
+sudo apt-get -y install mariadb-server
+sudo apt-get -y install nodejs npm
+sudo apt-get -y install spotify-client
 
 # Install phpmyadmin only after MySQL has been installed
-sudo apt-get install phpmyadmin
+sudo apt-get -y install phpmyadmin
 
 # Install pacapt (https://github.com/icy/pacapt)
 sudo wget -O /usr/local/bin/pacman https://github.com/icy/pacapt/raw/master/pacman
